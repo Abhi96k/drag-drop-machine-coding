2a18df35a6ccd5997d36181b85b51a91ff4f6bc0
ABHISHEK NANGARE <88939013+Abhi96k@users.noreply.github.com>
2025-10-04 13:02:59 +0000

    Adding ref to avoid rernder and access the child dom by parent

--- src/componets/Note.jsx
+++ src/componets/Note.jsx
@@ -1,5 +1,27 @@
-const Note = ({ content }) => {
-  return <div className="note">{content.text}</div>;
-};
+import { forwardRef } from "react";
+
+const Note = forwardRef(({ content, initialPos, ...props }, ref) => {
+  return (
+    <div
+      ref={ref}
+      className="note"
+      style={{
+        position: "absolute",
+        left: `${initialPos?.x}px`,
+        top: `${initialPos?.y}px`,
+        border: "1px solid black",
+        userSelect: "none",
+        width: "200px",
+        cursor: "move",
+        padding: "8px",
+        backgroundColor: "lightgoldenrodyellow",
+        borderRadius: "4px",
+      }}
+      {...props}
+    >
+      📍{content.text}
+    </div>
+  );
+});
 
 export default Note;
--- src/componets/Notes.jsx
+++ src/componets/Notes.jsx
@@ -1,10 +1,54 @@
+import { useEffect, useRef } from "react";
 import Note from "./Note";
+import React from "react";
 
 const Notes = ({ notes = [], setNotes = () => {} }) => {
+  const noteRefs = useRef([]);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+
+    const updatedNotes = notes.map((note) => {
+      const savedNote = savedNotes.find((n) => n.id === note.id);
+      if (savedNote) return savedNote;
+
+      const position = determineNewPosition();
+      return { ...note, position };
+    });
+
+    setNotes(updatedNotes);
+    localStorage.setItem("notes", JSON.stringify(updatedNotes));
+  }, []);
+
+  const determineNewPosition = () => {
+    const maxX = window.innerWidth - 250;
+    const maxY = window.innerHeight - 150;
+    return {
+      x: Math.floor(Math.random() * maxX),
+      y: Math.floor(Math.random() * maxY),
+    };
+  };
+
+  const handleDargeStart = (id, e) => {
+    const noteRef = noteRefs.current[id].current;
+  };
+
   return (
     <div>
       {notes.map((note) => (
-        <Note key={note.id} content={note} />
+        <Note
+          key={note.id}
+          content={note}
+          initialPos={note.position}
+          ref={
+            noteRefs.current[note.id]
+              ? noteRefs.current[note.id]
+              : (noteRefs.current[note.id] = React.createRef())
+          }
+          onMouseDown={() => {
+            handleDargeStart(note.id, e);
+          }}
+        />
       ))}
     </div>
   );
